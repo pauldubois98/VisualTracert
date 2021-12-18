7a2755921ba2292cedf070cdf1618f920668f667
Paul Dubois <pauldubois98@gmail.com>
2021-12-18 11:04:05 +0100

    added read ips from tracert file func

--- GUI.py
+++ GUI.py
@@ -2,6 +2,27 @@ import tkinter as tk
 
 
 
+def get_ips(filename):
+    f = open(filename, 'r')
+    ls =  f.readlines()
+    ips = []
+    i=4
+    l = ls[i]
+    while i<len(ls) and l!='\n' and l!='':
+        
+        if '[' in l and '[' in l:
+            a = l.find('[')
+            b = l.find(']')
+            ips.append(l[a+1:b])
+        else:
+            ips.append(l[33:].rstrip())
+        i+=1
+        l = ls[i]
+    f.close()
+    return ips
+
+
+
 root = tk.Tk()
 root.title("Visual tracert")
 adress_entry = tk.Entry(root, width=60, justify='center')
